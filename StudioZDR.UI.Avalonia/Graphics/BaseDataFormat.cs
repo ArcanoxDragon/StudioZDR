7ec39b2abd95aa6239c7471993e243b95e5f487b
Arcanox <arcanox@arcanox.me>
2025-04-07 02:31:17 -0500

    Add entire collection of code to read BCTEX files from Dread RomFS

---
+++ StudioZDR.UI.Avalonia/Graphics/BaseDataFormat.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using Overby.Extensions.AsyncBinaryReaderWriter;
+
+namespace StudioZDR.UI.Avalonia.Graphics;
+
+internal abstract class BaseDataFormat
+{
+	public async Task ReadAsync(Stream stream, CancellationToken cancellationToken = default)
+	{
+		using var reader = new AsyncBinaryReader(stream, Encoding.UTF8, leaveOpen: true);
+
+		await ReadAsync(reader, cancellationToken).ConfigureAwait(false);
+	}
+
+	public abstract Task ReadAsync(AsyncBinaryReader reader, CancellationToken cancellationToken = default);
+}
